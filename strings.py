036aca68b4466d70a8bec21db1a430a2bdf2dd03
Salam Waddah <salamwaddah@gmail.com>
2020-08-22 22:59:50 +0400

    strings

---
+++ strings.py
@@ -0,0 +1,9 @@
+phrase = "Giraffe Academy"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("f"))
+print(phrase.replace("Giraffe", "Salam"))
