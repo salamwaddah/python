38b257dd4cda7ec56c05ead5f43d3f41824539c9
Salam Waddah <salamwaddah@gmail.com>
2020-08-22 22:54:01 +0400

    variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+name = "Salam"
+age = "25"
+
+print("There once was a man named " + name + ",")
+print("he was " + age + " years old. ")
+print("He really liked the name " + name + ",")
+print("but didn't like being " + age + ".")
