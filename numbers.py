ce1a9a7b88e03b0cfe3fa9b8fc2e6940bc7d780f
Salam Waddah <salamwaddah@gmail.com>
2020-08-22 23:09:21 +0400

    numbers

---
+++ numbers.py
@@ -0,0 +1,15 @@
+from math import *
+
+print(3 * (4.5 + 5))
+print(10 % 3)
+print(str(24) + " is my favorite number")
+
+negative_number = -24
+print(abs(negative_number))  # absolute
+print(pow(3, 2))  # power
+print(min(1, 2, 3))
+print(max(1, 2, 3))
+print(round(3.5))
+print(ceil(3.8))
+print(floor(3.8))
+print(sqrt(2))
