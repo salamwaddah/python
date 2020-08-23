3d852e6cb0c8815474eff64b291b1fce4a81ba1b
Salam Waddah <salamwaddah@gmail.com>
2020-08-23 10:38:00 +0400

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions["Oct"])
+print(monthConversions.get("oct", "Invalid month"))
+print(monthConversions)
