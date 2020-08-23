ced23e9c666fe26131bfe3312fb403740d468855
Salam Waddah <salamwaddah@gmail.com>
2020-08-23 10:53:34 +0400

    for

---
+++ for.py
@@ -0,0 +1,15 @@
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends = ["Kevin", "Karen", "Jim", "Jim", "Jim", "Oscar", "Toby"]
+for friend in friends:
+    print(friend)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print("First iteration")
+    else:
+        print("Not first iteration")
