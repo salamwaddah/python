ef9a6cc28f09096a8b46106af4670404f79da7fe
Salam Waddah <salamwaddah@gmail.com>
2020-08-24 22:27:38 +0400

    classes and objects

---
+++ classes.py
@@ -0,0 +1,7 @@
+from classes.Student import Student
+
+salam = Student("Salam", "CS", 1.25, False)
+chessy = Student("Chessy", "BSN", 2.0, False)
+
+print(salam.name)
+print(chessy.gwa)
---
+++ classes/Student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, major, gwa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gwa = gwa
+        self.is_on_probation = is_on_probation
