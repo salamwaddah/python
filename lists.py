bd74f1c186228eec49107ee53cbf0a02ac3ef50d
Salam Waddah <salamwaddah@gmail.com>
2020-08-24 21:25:59 +0400

    2d lists

--- lists.py
+++ lists.py
@@ -26,3 +26,17 @@ print(friends)
 friends.clear()
 print(friends)
 print(friends_copy)
+
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+print(number_grid[0][0])
+print(number_grid[2][1])
+
+for row in number_grid:
+    for col in row:
+        print(col)
